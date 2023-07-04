51575b950d9eddf325cb11f2175e0ceedbead02c
Md. Shofiul Islam <ytshofiul@gmail.com>
2023-07-05 00:20:31 +0600

    nest pipe added

--- src/tasks/dtos/task.dto.ts
+++ src/tasks/dtos/task.dto.ts
@@ -1,5 +1,13 @@
+import { IsEmpty, IsNotEmpty } from "class-validator";
+
+
 export class CreateTaskDto {
+    @IsNotEmpty()
     title: string;
+
+    @IsNotEmpty()
     description: string;
+    
+    @IsEmpty()
     createdBy: string;
 }
